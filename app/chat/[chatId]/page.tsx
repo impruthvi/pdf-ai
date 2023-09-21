69b7a94cbbf0892da95d2beef6798bb377a784ce
Pruthvisinh Rajput <61496236+impruthvi@users.noreply.github.com>
2023-09-21 10:01:49 +0530

    Fixed some style issues

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -22,18 +22,18 @@ const ChatPage: React.FC<Props> = async ({ params: { chatId } }) => {
 
     if(!_chats.find(chat => chat.id === parseInt(chatId))) return redirect('/');
 
-  return <div className="flex max-h-screen overflow-scroll">
+  return <div className="flex max-h-screen">
     <div className="flex w-full max-h-screen overflow-scroll">
         {/* Chat sidebar */}
-        <div className="flex-[1] mx-w-xs">
+        <div className="flex-[1] max-w-xs">
             <ChatSidebar chats={_chats} chatId={parseInt(chatId)}/>
         </div>
         {/* PDF viewer */}
-        <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+        <div className="max-h-screen p-4 oveflow-scroll flex-[5]">
             {/* <PdfViewer /> */}
         </div>
         {/* Chat Component */}
-        <div className="flex-[3] border-l-4 border-l-slate-250">
+        <div className="flex-[3] border-l-4 border-l-slate-200">
             {/* <ChatComponent /> */}
         </div>
     </div>
